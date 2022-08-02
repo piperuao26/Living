d090beee639e6ba4b98d590817a3db2a98e2c451
Andres <piperuao26@gmail.com>
2022-08-02 16:38:24 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Juan Esteban Pavas González'})
